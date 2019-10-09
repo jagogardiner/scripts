9c3c0ae13e4b62db690aa37c7b8e1cff08ff6c03
nysascape <nysadev@raphielgang.org>
2019-10-09 02:10:26 +0000

    scripts: just make sure our enviroments vars are exported and F
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- envsetup.sh
+++ envsetup.sh
@@ -19,25 +19,47 @@ if [[ "$*" =~ "clang"* ]]; then
         git clone https://github.com/RaphielGang/aarch64-linux-gnu-8.x.git --depth=1 "${KERNELDIR}/gcc"
         git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi/ -b 240719 --depth=1 "${KERNELDIR}/gcc32"
         git clone https://github.com/RaphielGang/aosp-clang --depth=1 "${KERNELDIR}"/clang
-        export COMPILER_STRING='AOSP Clang (latest)'
-	export COMPILER_TYPE='clang'
+        COMPILER_STRING='AOSP Clang (latest)'
+	COMPILER_TYPE='clang'
 elif [[ "$*" =~ "gcc10"* ]]; then
         git clone https://github.com/RaphielGang/aarch64-raph-linux-android -b elf --depth=1 "${KERNELDIR}/gcc"
         git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi/ -b 240719 --depth=1 "${KERNELDIR}/gcc32"
-        export COMPILER_STRING='GCC 10 (experimental)'
-	export COMPILER_TYPE='GCC10'
+        COMPILER_STRING='GCC 10 (experimental)'
+	COMPILER_TYPE='GCC10'
 elif [[ "$*" =~ "gcc4.9"* ]]; then
         git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 "${KERNELDIR}/gcc"
         cd gcc || exit
         # Google is a hoe
         git reset --hard 75c0ace0eb9ba47c11df56971e7f63f2ebaa9fbd
         cd ..
-        export COMPILER_STRING='GCC 4.9 from Google'
-	export COMPILER_TYPE='GCC4.9'
+        COMPILER_STRING='GCC 4.9 from Google'
+	COMPILER_TYPE='GCC4.9'
 else
         # Default to GCC from kdrag0n
         git clone https://github.com/kdrag0n/aarch64-elf-gcc -b 9.x --depth=1 "${KERNELDIR}/gcc"
         git clone https://github.com/kdrag0n/arm-eabi-gcc -b 9.x --depth=1 "${KERNELDIR}/gcc32"
-        export COMPILER_STRING='GCC 9.x'
-	export COMPILER_TYPE='GCC9.x'
+        COMPILER_STRING='GCC 9.x'
+	COMPILER_TYPE='GCC9.x'
 fi
+
+export COMPILER_STRING COMPILER_TYPE KERNELDIR SCRIPTS OUTDIR
+
+# Export Telegram.sh
+TELEGRAM=${SCRIPTS}/telegram/telegram
+
+# Examine our compilation threads
+# 2x of our available CPUs
+# Kanged from @raphielscape <3
+CPU="$(grep -c '^processor' /proc/cpuinfo)"
+JOBS="$(( CPU * 2 ))"
+
+# Export our Telegram chat ID(s) - more than one is used
+CI_CHANNEL="-1001420038245"
+TG_GROUP="-1001435271206"
+
+# Parse git things
+PARSE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+PARSE_ORIGIN="$(git config --get remote.origin.url)"
+COMMIT_POINT="$(git log --pretty=format:'%h : %s' -1)"
+
+export TELEGRAM JOBS CH_CHANNEL TG_GROUP PARSE_BRANCH PARSE_ORIGIN COMMIT_POINT
