070641e3a8cebbbecd8b2e81f5b565964bbb7723
nysascape <nysadev@raphielgang.org>
2019-10-09 00:44:33 +0000

    treewide: Just huge Semaphore updates ig
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- envsetup.sh
+++ envsetup.sh
@@ -11,30 +11,54 @@
 # Export home as en environment-wide thingy
 export HOME=${SEMAPHORE_PROJECT_DIR}/..
 export SCRIPTS=${HOME}/scripts
+export KERNELDIR=${SEMAPHORE_PROJECT_DIR}
+export OUTDIR=${KERNELDIR}/out
 
 # Pick your poison
 if [[ "$*" =~ "clang"* ]]; then
         git clone https://github.com/RaphielGang/aarch64-linux-gnu-8.x.git --depth=1 "${HOME}/gcc"
         git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi/ -b 240719 --depth=1 "${HOME}/gcc32"
         git clone https://github.com/RaphielGang/aosp-clang --depth=1 "${HOME}"/clang
-        export COMPILER='AOSP Clang (latest)'
+        export COMPILER_STRING='AOSP Clang (latest)'
+	export COMPILER_TYPE='clang'
 elif [[ "$*" =~ "gcc10"* ]]; then
         git clone https://github.com/RaphielGang/aarch64-raph-linux-android -b elf --depth=1 "${HOME}/gcc"
         git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi/ -b 240719 --depth=1 "${HOME}/gcc32"
-        export COMPILER='GCC 10 (experimental)'
+        export COMPILER_STRING='GCC 10 (experimental)'
+	export COMPILER_TYPE='GCC10'
 elif [[ "$*" =~ "gcc4.9"* ]]; then
         git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 "${HOME}/gcc"
         cd gcc || exit
         # Google is a hoe
         git reset --hard 75c0ace0eb9ba47c11df56971e7f63f2ebaa9fbd
         cd ..
-        export COMPILER='GCC 4.9 from Google'
+        export COMPILER_STRING='GCC 4.9 from Google'
+	export COMPILER_TYPE='GCC4.9'
 else
         # Default to GCC from kdrag0n
         git clone https://github.com/kdrag0n/aarch64-elf-gcc -b 9.x --depth=1 "${HOME}/gcc"
         git clone https://github.com/kdrag0n/arm-eabi-gcc -b 9.x --depth=1 "${HOME}/gcc32"
-        export COMPILER='GCC 9.x'
+        export COMPILER_STRING='GCC 9.x'
+	export COMPILER_TYPE='GCC9.x'
 fi
 
+# Export Telegram.sh
 export telegram=${SCRIPTS}/telegram/telegram
 
+# Examine our compilation threads
+# 2x of our available CPUs
+# Kanged from @raphielscape <3
+CPU="$(grep -c '^processor' /proc/cpuinfo)"
+export JOBS="$(( CPU * 2 ))"
+
+# Export our Telegram chat ID(s) - more than one is used
+export CI_CHANNEL="-1001420038245"
+export TG_GROUP="-1001435271206"
+
+# Parse git things
+export PARSE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+export PARSE_ORIGIN="$(git config --get remote.origin.url)"
+export COMMIT_POINT="$(git log --pretty=format:'%h : %s' -1)"
+
+# At the end of it all, we need to be in our kernel dir
+cd ${KERNELDIR}
--- semaphore.sh
+++ semaphore.sh
@@ -7,20 +7,106 @@
 #
 # Semaphore build script for Acrux
 
-# Enviroment variables
-export KERNELDIR=${SEMAPHORE_PROJECT_DIR}
-
-# Check if Clang is needed
-if [[ "$@" =~ "clang" ]]; then
-	export CC=Clang
-else
-	export CC=GCC
-fi
-
-# Just install proper packages
-sudo install-package --update-new ccache bc bash git-core gnupg build-essential \
-		zip curl make automake autogen autoconf autotools-dev libtool shtool python \
-		m4 gcc libtool zlib1g-dev dash pigz
-
 # Just to make sure bc Semaphore can be drunk
 cd ${KERNELDIR} 
+
+# Make sure our branch is availiable
+BRANCH=${PARSE_BRANCH}
+
+# Make sure our fekking token is exported ig?
+export TELEGRAM_TOKEN=${BOT_API_TOKEN}
+
+# Some misc enviroment vars
+DEVICE=Platina
+CIPROVIDER=Semaphore
+
+# Clone our AnyKernel3 branch to HOME
+git clone https://github.com/nysascape/Acrux-AK3 -b master ${HOME}/anykernel3
+export ANYKERNEL = ${HOME}/anykernel3
+
+# Do some silly defconfig replacements
+if [[ "${BRANCH}" =~ "staging"* ]]; then
+	# For staging branch
+	export KERNELTYPE=nightly
+	export KERNELNAME="Acrux-${RELEASE_VERSION}-Nightly-${COMPILER_TYPE}-$(date +%Y%m%d-%H%M)"
+	sed -i "50s/.*/CONFIG_LOCALVERSION="${KERNELNAME}"/g" arch/arm64/configs/acrux_defconfig
+elif [[ "${BRANCH} =~ "pie"* ]]; then
+	# For stable (pie) branch
+	export KERNELTYPE=stable
+	export KERNELNAME="Acrux-${RELEASE_VERSION}-Release-${COMPILER_TYPE}-$(date +%Y%m%d-%H%M)"
+	sed -i "50s/.*/CONFIG_LOCALVERSION="${KERNELNAME}"/g" arch/arm64/configs/acrux_defconfig
+else
+	# Dunno when this will happen but we will cover, just in case
+	export KERNELTYPE=${BRANCH}
+	export KERNELNAME="Acrux-${RELEASE_VERSION}-${BRANCH}-${COMPILER_TYPE}-$(date +%Y%m%d-%H%M)"
+	sed -i "50s/.*/CONFIG_LOCALVERSION="${KERNELNAME}"/g" arch/arm64/configs/acrux_defconfig
+fi
+
+# Might as well export our zip
+export ZIPNAME="${KERNELNAME}.zip"
+
+# Send to main group
+tg_groupcast() {
+    "${TELEGRAM}" -c ${TG_GROUP} -H \
+    "$(
+		for POST in "${@}"; do
+			echo "${POST}"
+		done
+    )"
+}
+
+# sendcast to channel
+tg_channelcast() {
+    "${TELEGRAM}" -c ${CI_CHANNEL} -H \
+    "$(
+		for POST in "${@}"; do
+			echo "${POST}"
+		done
+    )"
+}
+
+# Let's announce our naisu new kernel!
+tg_groupcast "Acrux ${KERNELTYPE} compilation clocked at $(date +%Y%m%d-%H%M)!"
+tg_channelcast "Compiler: <code>${COMPILER_STRING}</code>" \
+	"Device: <b>${DEVICE}</b>" \
+	"Branch: <code>${BRANCH}</code>" \
+	"Commit point: <code>${COMMIT_POINT}</code>" \
+	"Under <code>$(hostname)</code" \
+	"Clocked at: <code>$(date +%Y%m%d-%H%M)</code>" \
+	"Started on <code>$(whoami)</code>"
+
+# Make is shit so I have to pass thru some toolchains
+# Let's build, anyway
+PATH="${HOME}/clang/bin:${PATH}"
+START=$(date +"%s")
+make O=out ARCH=arm64 acrux_defconfig
+if [[ "${COMPILER_TYPE}" =~ "clang"* ]]; then
+        make -j${JOBS} O=out ARCH=arm64 CC=clang CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="${HOME}/gcc/bin/aarch64-linux-gnu-" CROSS_COMPILE_ARM32="${HOME}/gcc32/bin/arm-maestro-linux-gnueabi-"
+elif [[ "${COMPILER_TYPE}" =~ "GCC10"* ]]; then
+	make -j${JOBS} O=out ARCH=arm64 CROSS_COMPILE="${HOME}/gcc/bin/aarch64-raphiel-elf-" CROSS_COMPILE_ARM32="${HOME}/gcc32/bin/arm-maestro-linux-gnueabi-"
+elif [[ "${COMPILER_TYPE}" =~ "GCC4.9"* ]]; then
+	make -j${JOBS} O=out ARCH=arm64 CROSS_COMPILE="${HOME}/gcc/bin/aarch64-linux-android-"
+else
+	make -j${JOBS} O=out ARCH=arm64 CROSS_COMPILE="${HOME}/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="${HOME}/gcc32/bin/arm-eabi-"
+fi
+
+END=$(date +"%s")
+DIFF=$(( END - START ))
+
+# Copy our !!hopefully!! compiled kernel
+cp ${OUTDIR}/arch/arm64/boot/Image.gz-dtb ${ANYKERNEL}/
+
+# POST ZIP OR FAILURE
+cd ${ANYKERNEL}
+zip -r9 ${ZIPNAME} *
+CHECKER=$(ls -l ${ZIPNAME} | awk '{print $5}')
+
+if (($((CHECKER / 1048576)) > 5)); then
+	"${TELEGRAM}" -f $ZIPNAME" -c ${CI_CHANNEL}
+	tg_channelcast "Build for ${DEVICE} with ${COMPILER_STRING} took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)!"
+	tg_groupcast "Build for ${DEVICE} with ${COMPILER_STRING} took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! @acruxci"
+else
+	tg_channelcast "Build for ${DEVICE} <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! Check Semaphore for errors!"
+	tg_groupcast "Build for ${DEVICE} <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! Check Semaphore for errors! @acruxci" 
+	exit 1;
+fi
