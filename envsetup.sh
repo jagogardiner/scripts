7bdeb0c06b0bb69f54a4bacd35a7d5054433c80a
nysascape <nysadev@raphielgang.org>
2020-02-17 20:15:24 +0000

    treewide: Uprev licence to 1.d
    Signed-off-by: nysascape <nysadev@raphielgang.org>
    Change-Id: I399d2c4654f3d65e3e3c6106790172ea714fd5b9
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- envsetup.sh
+++ envsetup.sh
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2019 nysascape
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
 # Probably the 3rd bad apple coming
