513bf7fd6d5c6ed43436ddbb0975199252af3d8a
nysascape <jago@nysascape.digital>
2020-03-09 23:11:01 +0000

    linuxsetup: A script to rule them all
    Signed-off-by: nysascape <jago@nysascape.digital>
    Change-Id: I90a87907ad4a039b103c2b41eb131a2aefeb6d27

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -6,40 +6,55 @@
 # you may not use this file except in compliance with the License.
 #
 # Let's install some packages
-sudo apt-get -y install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev zsh
+if [[ "$*" =~ "arch"* ]]; then
+	sudo pacman -S zsh adb fastboot curl git code neofetch iwd dhcpcd gnome-terminal lightdm gnome-backgrounds lightdm-gtk-greeter telegram-desktop ttf-opensans
+else
+	sudo apt-get -y install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev zsh
+fi
+
+# Run oh-my-zsh installer unatteneded
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Setup Pure ZSH theme
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+echo 'fpath+=("$HOME/.zsh/pure")' >> ~/.zshrc
+echo 'autoload -U promptinit; promptinit' >> ~/.zshrc
+echo 'prompt pure' >> ~/.zshrc
 
 # Clone in my aliases
-git clone https://github.com/nysascape/aliases ~/aliases
-sed -i 's#.bash_aliases#aliases/aliases#g' ~/.bashrc
+git clone https://github.com/nysascape/aliases ~/.aliases
+echo "source ~/.aliases/aliases" >> ~/.zshrc
 
 # Git configurations
 git config --global user.name "nysascape"
-git config --global user.email "nysadev@raphielgang.org"
+git config --global user.email "jago@nysascape.digital"
 git config --global credential.helper store
+git config --global commit.gpgsign true
+git config --global user.signingkey "A15571E738CE3CD4"
 
 # GCC 9 is always a good thing to have
 git clone https://github.com/kdrag0n/aarch64-elf-gcc -b 9.x --depth=1 ~/gcc9
 git clone https://github.com/kdrag0n/arm-eabi-gcc -b 9.x --depth=1 ~/gcc932
 
-# Clone repo (thanks to LineageOS Wiki)
-mkdir -p ~/bin
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-chmod a+x ~/bin/repo
-
-# Add platform tools to PATH
-wget -O ~/tools.zip https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-unzip -d ~/.platform_tools ~/tools.zip
-echo "if [ -d "$HOME/.platform_tools/platform-tools" ] ; then" >> ~/.profile
-echo "    PATH="$HOME/.platform_tools/platform-tools:$PATH"" >> ~/.profile
-echo "fi" >> ~/.profile
-source ~/.profile
-rm ~/tools.zip
-
-# Source our zshrc just so our aliases take effect
-source ~/.zshrc
-
 # Add the Gerrit change-id hook
-mkdir ~/.git/hooks
+mkdir -p ~/.git/hooks
 git config --global core.hooksPath ~/.git/hooks
 curl -Lo ~/.git/hooks/commit-msg https://review.aosip.dev/tools/hooks/commit-msg
 chmod u+x ~/.git/hooks/commit-msg
+
+# Change shell to ZSH
+chsh -s "$zsh"
+
+# Arch stuff
+if [[ "$*" =~ "arch"* ]]; then
+	# Install aurpkg
+	git clone --depth=1 https://aur.archlinux.org/aurpkg.git /tmp/aurpkg
+	cd /tmp/aurpkg
+	makepkg -si
+	cd ~
+
+	# Install Google Chrome
+	aurpkg -S google-chrome
+fi
+
