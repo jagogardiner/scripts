a98e61a1771a470f370068095df1309113a19a86
nysascape <jago@nysascape.digital>
2020-04-09 02:16:11 +0100

    Circle: Revamp scripts completely
    * Cleanup codestyle
    * Removed unused compilers
    * Stop hardcoding kernel versions, names, etc
    
    Change-Id: I7c89e18f2548af1f581cd9c671f5ad8c3b330466
    Signed-off-by: nysascape <jago@nysascape.digital>

--- circle.sh
+++ circle.sh
@@ -1,68 +1,59 @@
 #!/usr/bin/env bash
-#
-# Copyright (C) 2019 nysascape
+# Copyright (C) 2019-2020 Jago Gardiner (nysascape)
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-# CI build script for Acrux
+# CI build script
 
-# Make sure our fekking token is exported ig?
+# Needed exports
 export TELEGRAM_TOKEN=${BOT_API_TOKEN}
+export ANYKERNEL=$(pwd)/anykernel3
 
-# Some misc enviroment vars
+# Avoid hardcoding things
+KERNEL=Acrux
+DEFCONFIG=acrux_defconfig
 DEVICE=Platina
 CIPROVIDER=CircleCI
 KERNELFW=Global
-
-# Clone our AnyKernel3 branch to KERNELDIR
-git clone https://github.com/nysascape/Acrux-AK3 -b master anykernel3
-export ANYKERNEL=$(pwd)/anykernel3
-
-# Parse git things
 PARSE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 PARSE_ORIGIN="$(git config --get remote.origin.url)"
 COMMIT_POINT="$(git log --pretty=format:'%h : %s' -1)"
 
-# Do some silly defconfig replacements
-if [[ "${PARSE_BRANCH}" =~ "staging"* ]]; then
-	# For staging branch
-	KERNELTYPE=nightly
-	KERNELNAME="Acrux-${KERNELRELEASE}-Nightly-${KERNELFW}-$(date +%y%m%d-%H%M)"
-	sed -i "50s/.*/CONFIG_LOCALVERSION=\"-${KERNELNAME}\"/g" arch/arm64/configs/acrux_defconfig
-        # Disable LTO on non-release builds
-        sed -i 's/CONFIG_LTO=y/CONFIG_LTO=n/g' arch/arm64/configs/acrux_defconfig
-        sed -i 's/# CONFIG_LTO_NONE is not set/CONFIG_LTO_NONE=y/g' arch/arm64/configs/acrux_defconfig
-        sed -i 's/CONFIG_LTO_CLANG=y/CONFIG_LTO_CLANG=n/g' arch/arm64/configs/acrux_defconfig
-elif [[ "${PARSE_BRANCH}" =~ "ten"* ]]; then
-	# For stable (ten) branch
-	KERNELTYPE=stable
-	KERNELNAME="Acrux-${KERNELRELEASE}-Release-${KERNELFW}-$(date +%y%m%d-%H%M)"
-        sed -i "50s/.*/CONFIG_LOCALVERSION=\"-${KERNELNAME}\"/g" arch/arm64/configs/acrux_defconfig
-else
-	# Dunno when this will happen but we will cover, just in case
-	KERNELTYPE=${PARSE_BRANCH}
-	KERNELNAME="Acrux-${KERNELRELEASE}-${PARSE_BRANCH}-${KERNELFW}-$(date +%y%m%d-%H%M)"
-        sed -i "50s/.*/CONFIG_LOCALVERSION=\"-${KERNELNAME}\"/g" arch/arm64/configs/acrux_defconfig
-fi
+# Kernel groups
+CI_CHANNEL=-1001420038245
+TG_GROUP=-1001435271206
 
-export KERNELTYPE KERNELNAME
+# Clang is annoying
+PATH="${KERNELDIR}/clang/bin:${PATH}"
 
-sed -i 's/CONFIG_LTO=y/# CONFIG_LTO is not set/g' arch/arm64/configs/acrux_defconfig
+# Kernel revision
+KERNELRELEASE=v2-ten
 
-# Workaround for long af kernel strings
-git config --global user.name "nysascape"
-git config --global user.email "nysadev@raphielgang.org"
-git add .
-git commit -m "stop adding dirty"
+# Function to replace defconfig versioning
+setversioning() {
+    if [[ "${PARSE_BRANCH}" =~ "staging"* ]]; then
+    	# For staging branch
+	    KERNELTYPE=nightly
+	    KERNELNAME="${KERNEL}-${KERNELRELEASE}-Nightly-${KERNELFW}-$(date +%y%m%d-%H%M)"
+	    sed -i "50s/.*/CONFIG_LOCALVERSION=\"-${KERNELNAME}\"/g" arch/arm64/configs/${DEFCONFIG}
+    elif [[ "${PARSE_BRANCH}" =~ "ten"* ]]; then
+	    # For stable (ten) branch
+	    KERNELTYPE=stable
+	    KERNELNAME="${KERNEL}-${KERNELRELEASE}-Release-${KERNELFW}-$(date +%y%m%d-%H%M)"
+        sed -i "50s/.*/CONFIG_LOCALVERSION=\"-${KERNELNAME}\"/g" arch/arm64/configs/${DEFCONFIG}
+    else
+	    # Dunno when this will happen but we will cover, just in case
+	    KERNELTYPE=${PARSE_BRANCH}
+	    KERNELNAME="${KERNEL}-${KERNELRELEASE}-${PARSE_BRANCH}-${KERNELFW}-$(date +%y%m%d-%H%M)"
+        sed -i "50s/.*/CONFIG_LOCALVERSION=\"-${KERNELNAME}\"/g" arch/arm64/configs/${DEFCONFIG}
+    fi
 
-# Might as well export our zip
-export TEMPZIPNAME="${KERNELNAME}-unsigned.zip"
-export ZIPNAME="${KERNELNAME}.zip"
-
-# Our TG channels
-export CI_CHANNEL="-1001420038245"
-export TG_GROUP="-1001435271206"
+    # Export our new localversion and zipnames
+    export KERNELTYPE KERNELNAME
+    export TEMPZIPNAME="${KERNELNAME}-unsigned.zip"
+    export ZIPNAME="${KERNELNAME}.zip"
+}
 
 # Send to main group
 tg_groupcast() {
@@ -74,7 +65,7 @@ tg_groupcast() {
     )"
 }
 
-# sendcast to channel
+# Send to channel
 tg_channelcast() {
     "${TELEGRAM}" -c "${CI_CHANNEL}" -H \
     "$(
@@ -84,130 +75,88 @@ tg_channelcast() {
     )"
 }
 
-# Let's announce our naisu new kernel!
-tg_groupcast "Acrux compilation clocked at $(date +%Y%m%d-%H%M)!"
+# Fix long kernel strings
+kernelstringfix() {
+    git config --global user.name "nysascape"
+    git config --global user.email "nysadev@raphielgang.org"
+    git add .
+    git commit -m "stop adding dirty"
+}
+
+# Make the kernel
+makekernel() {
+    # Clean any old AnyKernel
+    rm -rf ${ANYKERNEL}
+    git clone https://github.com/nysascape/Acrux-AK3 -b master anykernel3
+    kernelstringfix
+    START=$(date +"%s")
+    make O=out ARCH=arm64 ${DEFCONFIG}
+    if [[ "${COMPILER_TYPE}" =~ "clang"* ]]; then
+        make -j$(nproc --all) CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- O=out ARCH=arm64
+    else
+	    make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-eabi-"
+    fi
+
+    # Check if compilation is done successfully.
+    if ! [ -f "${OUTDIR}"/arch/arm64/boot/Image.gz-dtb ]; then
+	    END=$(date +"%s")
+	    DIFF=$(( END - START ))
+	    echo -e "Kernel compilation failed, See buildlog to fix errors"
+	    tg_channelcast "Build for ${DEVICE} (${KERNELFW}) <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! Check ${CIPROVIDER} for errors!"
+	    tg_groupcast "Build for ${DEVICE} (${KERNELFW}) <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! Check ${CIPROVIDER} for errors @nysascape! @nysaci"
+	    exit 1
+    fi
+}
+
+# Ship the compiled kernel
+shipkernel() {
+    # Copy compiled kernel
+    cp "${OUTDIR}"/arch/arm64/boot/Image.gz-dtb "${ANYKERNEL}"/
+
+    # Zip the kernel, or fail
+    cd "${ANYKERNEL}" || exit
+    zip -r9 "${TEMPZIPNAME}" *
+
+    # Sign the zip before sending it to Telegram
+    curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
+    java -jar zipsigner-3.0.jar ${TEMPZIPNAME} ${ZIPNAME}
+
+    # Ship it to the CI channel
+    "${TELEGRAM}" -f "$ZIPNAME" -c "${CI_CHANNEL}"
+
+    # Go back for any extra builds
+    cd ..
+}
+
+# Ship China firmware builds
+setchinafw() {
+    KERNELFW=China
+    # Pick DSP change
+    git cherry-pick 23dda5dd32a62488862985d7efc9d148e7f527f5
+}
+
+# Fix for CI builds running out of memory
+fixcilto() {
+    sed -i 's/CONFIG_LTO=y/# CONFIG_LTO is not set/g' arch/arm64/configs/${DEFCONFIG}
+    sed -i 's/CONFIG_LD_DEAD_CODE_DATA_ELIMINATION=y/# CONFIG_LD_DEAD_CODE_DATA_ELIMINATION is not set/g' arch/arm64/configs/${DEFCONFIG}
+}
+
+
+## Start the kernel buildflow ##
+setversioning
+fixcilto
+tg_groupcast "${KERNEL} compilation clocked at $(date +%Y%m%d-%H%M)!"
 tg_channelcast "Compiler: <code>${COMPILER_STRING}</code>" \
 	"Device: <b>${DEVICE}</b>" \
-	"Kernel: <code>Acrux, release ${KERNELRELEASE}</code>" \
+	"Kernel: <code>${KERNEL}, release ${KERNELRELEASE}</code>" \
 	"Branch: <code>${PARSE_BRANCH}</code>" \
 	"Commit point: <code>${COMMIT_POINT}</code>" \
-	"Under <code>${CIPROVIDER}</code>" \
-	"Clocked at: <code>$(date +%Y%m%d-%H%M)</code>" \
-	"Started on <code>$(whoami)</code>"
-
-# Make is shit so I have to pass thru some toolchains
-# Let's build, anyway
-PATH="${KERNELDIR}/clang/bin:${PATH}"
-START=$(date +"%s")
-
-mkdir ${KERNELDIR}/out
-
-make O=out ARCH=arm64 acrux_defconfig
-if [[ "${COMPILER_TYPE}" =~ "clang"* ]]; then
-        make -j"${JOBS}" CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- O=out ARCH=arm64
-elif [[ "${COMPILER_TYPE}" =~ "GCC10"* ]]; then
-	make -j"${JOBS}" O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-raphiel-elf-" CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-maestro-linux-gnueabi-"
-elif [[ "${COMPILER_TYPE}" =~ "GCC4.9"* ]]; then
-	make -j"${JOBS}" O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-linux-android-"
-else
-	make -j"${JOBS}" O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-eabi-"
-fi
-
-## Check if compilation is done successfully.
-if ! [ -f "${OUTDIR}"/arch/arm64/boot/Image.gz-dtb ]; then
-	END=$(date +"%s")
-	DIFF=$(( END - START ))
-	echo -e "Kernel compilation failed, See buildlog to fix errors"
-	tg_channelcast "Build for ${DEVICE} <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! Check ${CIPROVIDER} for errors!"
-	tg_groupcast "Build for ${DEVICE} <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! Check ${CIPROVIDER} for errors @nysascape! @nysaci"
-	exit 1
-fi
-
-# Copy our !!hopefully!! compiled kernel
-cp "${OUTDIR}"/arch/arm64/boot/Image.gz-dtb "${ANYKERNEL}"/
-
-# POST ZIP OR FAILURE
-cd "${ANYKERNEL}" || exit
-zip -r9 "${TEMPZIPNAME}" *
-
-## Sign the zip before sending it to telegram
-curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
-java -jar zipsigner-3.0.jar ${TEMPZIPNAME} ${ZIPNAME}
-
-"${TELEGRAM}" -f "$ZIPNAME" -c "${CI_CHANNEL}"
-
-cd ..
-
-rm -rf "${ANYKERNEL}"
-git clone https://github.com/nysascape/Acrux-AK3 -b master anykernel3
-
-# Build China fixes
-KERNELFW=China
-# Pick DSP change
-git cherry-pick 23dda5dd32a62488862985d7efc9d148e7f527f5
-
-# Do some silly defconfig replacements
-if [[ "${PARSE_BRANCH}" =~ "staging"* ]]; then
-        # For staging branch
-        KERNELTYPE=nightly
-        KERNELNAME="Acrux-${KERNELRELEASE}-Nightly-${KERNELFW}-$(date +%Y%m%d-%H%M)"
-        sed -i "50s/.*/CONFIG_LOCALVERSION=\"-${KERNELNAME}\"/g" arch/arm64/configs/acrux_defconfig
-        # Disable LTO on non-release builds
-        sed -i 's/CONFIG_LTO=y/CONFIG_LTO=n/g' arch/arm64/configs/acrux_defconfig
-        sed -i 's/# CONFIG_LTO_NONE is not set/CONFIG_LTO_NONE=y/g' arch/arm64/configs/acrux_defconfig
-        sed -i 's/CONFIG_LTO_CLANG=y/CONFIG_LTO_CLANG=n/g' arch/arm64/configs/acrux_defconfig
-
-elif [[ "${PARSE_BRANCH}" =~ "ten"* ]]; then
-        # For stable (ten) branch
-        KERNELTYPE=stable
-        KERNELNAME="Acrux-${KERNELRELEASE}-Release-${KERNELFW}-$(date +%Y%m%d-%H%M)"
-        sed -i "50s/.*/CONFIG_LOCALVERSION=\"-${KERNELNAME}\"/g" arch/arm64/configs/acrux_defconfig
-else
-        # Dunno when this will happen but we will cover, just in case
-        KERNELTYPE=${PARSE_BRANCH}
-        KERNELNAME="Acrux-${KERNELRELEASE}-${PARSE_BRANCH}-${KERNELFW}-$(date +%Y%m%d-%H%M)"
-        sed -i "51s/.*/CONFIG_LOCALVERSION=\"-${KERNELNAME}\"/g" arch/arm64/configs/acrux_defconfig
-fi
-
-export KERNELTYPE KERNELNAME
-
-# Might as well export our zip
-export TEMPZIPNAME="${KERNELNAME}-unsigned.zip"
-export ZIPNAME="${KERNELNAME}.zip"
-make O=out ARCH=arm64 acrux_defconfig
-if [[ "${COMPILER_TYPE}" =~ "clang"* ]]; then
-        make -j"${JOBS}" CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- O=out ARCH=arm64
-elif [[ "${COMPILER_TYPE}" =~ "GCC10"* ]]; then
-        make -j"${JOBS}" O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-raphiel-elf-" CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-maestro-linux-gnueabi-"
-elif [[ "${COMPILER_TYPE}" =~ "GCC4.9"* ]]; then
-        make -j"${JOBS}" O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-linux-android-"
-else
-        make -j"${JOBS}" O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-eabi-"
-fi
-
-## Check if compilation is done successfully.
-if ! [ -f "${OUTDIR}"/arch/arm64/boot/Image.gz-dtb ]; then
-	END=$(date +"%s")
-	DIFF=$(( END - START ))
-        echo -e "Kernel compilation failed !!(FOR CHINA FW)!!, See buildlog to fix errors"
-        tg_channelcast "Build for ${DEVICE} <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! Check ${CIPROVIDER} for errors!"
-        tg_groupcast "Build for ${DEVICE} <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! Check ${CIPROVIDER} for errors @nysascape! @nysaci"
-        exit 1
-fi
-
-# Copy our !!hopefully!! compiled kernel
-cp "${OUTDIR}"/arch/arm64/boot/Image.gz-dtb "${ANYKERNEL}"/
-
-# POST ZIP OR FAILURE
-cd "${ANYKERNEL}" || exit
-zip -r9 "${TEMPZIPNAME}" *
-
-## Sign the zip before sending it to telegram
-curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
-java -jar zipsigner-3.0.jar ${TEMPZIPNAME} ${ZIPNAME}
-
-"${TELEGRAM}" -f "$ZIPNAME" -c "${CI_CHANNEL}"
-
+	"Clocked at: <code>$(date +%Y%m%d-%H%M)</code>"
+makekernel || exit 1
+shipkernel
+setchinafw
+makekernel || exit 1
+shipkernel
 END=$(date +"%s")
 DIFF=$(( END - START ))
 tg_channelcast "Build for ${DEVICE} with ${COMPILER_STRING} took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)!"
--- envsetup.sh
+++ envsetup.sh
@@ -19,19 +19,6 @@ if [[ "$*" =~ "clang"* ]]; then
         git clone https://github.com/kdrag0n/proton-clang --depth=1 "${KERNELDIR}"/clang
         COMPILER_STRING='Proton Clang (latest)'
 	COMPILER_TYPE='clang'
-elif [[ "$*" =~ "gcc10"* ]]; then
-        git clone https://github.com/RaphielGang/aarch64-raph-linux-android -b elf --depth=1 "${KERNELDIR}/gcc"
-        git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi/ -b 240719 --depth=1 "${KERNELDIR}/gcc32"
-        COMPILER_STRING='GCC 10 (experimental)'
-	COMPILER_TYPE='GCC10'
-elif [[ "$*" =~ "gcc4.9"* ]]; then
-        git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 "${KERNELDIR}/gcc"
-        cd gcc || exit
-        # Google is a hoe
-        git reset --hard 75c0ace0eb9ba47c11df56971e7f63f2ebaa9fbd
-        cd ..
-        COMPILER_STRING='GCC 4.9 from Google'
-	COMPILER_TYPE='GCC4.9'
 else
         # Default to GCC from Arter
         git clone https://github.com/arter97/arm64-gcc --depth=1 "${KERNELDIR}/gcc"
@@ -43,18 +30,8 @@ fi
 export COMPILER_STRING COMPILER_TYPE KERNELDIR SCRIPTS OUTDIR
 
 git clone https://github.com/fabianonline/telegram.sh/ telegram
+
 # Export Telegram.sh
 TELEGRAM=${KERNELDIR}/telegram/telegram
-
-# Examine our compilation threads
-# 2x of our available CPUs
-# Kanged from @raphielscape <3
-CPU="$(grep -c '^processor' /proc/cpuinfo)"
-JOBS="$(( CPU * 2 ))"
-
-# Parse git things
-PARSE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-PARSE_ORIGIN="$(git config --get remote.origin.url)"
-COMMIT_POINT="$(git log --pretty=format:'%h : %s' -1)"
 
 export TELEGRAM JOBS
